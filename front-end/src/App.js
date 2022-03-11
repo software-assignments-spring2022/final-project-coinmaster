addf9096e1825f0587a3ee247c5da201765dc4b0
Aziz Muhammad <Aziz@Azizs-MacBook-Pro.local>
2022-03-11 16:59:25 -0500

    project frontend screens setup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import Navbar from './components/Navbar';
+//this line imports bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+//IMPORT YOUR MAIN COMPONENT HERE; 
+//FOR EXAMPLE IF YOU ARE WORKING ON REGISTER
+//import register from ....
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      {/*replace this line with you component, for example "<Register/>" */}
+      {/*Note: this won't actually won't be where your screen is going to live
+      this will probably be something like Routes, but for now this is how we can test locally. */}
     </div>
   );
 }
-
 export default App;
--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,8 +1,9 @@
-
+//this is incomplete, Aziz will finish later
 
 function Navbar(props) {
     return (
         <>
+        <button type="button" class="btn btn-secondary">Secondary</button>
             <ul>
                 <li><a href="default.asp">Home</a></li>
                 <li><a href="news.asp">Coin Master</a></li>
