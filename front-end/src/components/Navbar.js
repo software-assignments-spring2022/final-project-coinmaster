c3fa07d632b7557876b3937b2cd080099ad467cf
Aziz Muhammad <Aziz@Azizs-MacBook-Pro.local>
2022-03-11 11:41:27 -0500

    barebones Navba

---
+++ front-end/src/components/Navbar.js
@@ -0,0 +1,15 @@
+
+
+function Navbar(props) {
+    return (
+        <>
+            <ul>
+                <li><a href="default.asp">Home</a></li>
+                <li><a href="news.asp">Coin Master</a></li>
+                <li><a href="contact.asp">Logo</a></li>
+            </ul>
+        </>
+    )
+  }
+
+export default Navbar
