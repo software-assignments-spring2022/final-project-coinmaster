c1f5cebc08ca9ad8935eccbcb95e98ea3de88c57
Olivia Wang <lw2694@nyu.edu>
2022-03-21 12:24:33 -0400

    add video change style

--- front-end/src/components/screens/Learn/Video.js
+++ front-end/src/components/screens/Learn/Video.js
@@ -5,6 +5,8 @@ export default function Video() {
     <div className="App">
       <h4>Video: What is Bitcoin?</h4>
       <YoutubeEmbed embedId="41JCpzvnn_0" />
+      <h4>Video: Simple $100 a Day Crypto Trading Strategy Anyone Can Use</h4>
+      <YoutubeEmbed embedId="bxnZEgtHf5E" />
     </div>
   );
 }
