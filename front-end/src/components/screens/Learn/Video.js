7cc8f6120c05121663577a29f9a9124320f1a0d2
Olivia Wang <lw2694@nyu.edu>
2022-03-21 00:03:20 -0400

    video

---
+++ front-end/src/components/screens/Learn/Video.js
@@ -0,0 +1,10 @@
+import YoutubeEmbed from "./YoutubeEmbed";
+
+export default function Video() {
+  return (
+    <div className="App">
+      <h4>Video: What is Bitcoin?</h4>
+      <YoutubeEmbed embedId="41JCpzvnn_0" />
+    </div>
+  );
+}
