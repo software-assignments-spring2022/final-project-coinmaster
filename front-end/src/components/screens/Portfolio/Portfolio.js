f2fdf49052dfa8852d9711848159159be1feda25
Aziz Muhammad <Aziz@Azizs-MacBook-Pro.local>
2022-03-21 10:25:01 -0400

    stats table in portfolio

--- front-end/src/components/screens/Portfolio/Portfolio.js
+++ front-end/src/components/screens/Portfolio/Portfolio.js
@@ -5,6 +5,10 @@ import React, { useState } from 'react';
 function Portfolio(props){
 
     const [ownedCryptos,setOwnedCryptos] = useState([["1","Lorem","$100.00","$100M"]]);
+    const [netProfit,setNetProfit] = useState("5.34%");
+    const [allTimeHigh,setAllTimeHigh] = useState("12.44%");
+    const [yearHigh,setYearHigh] = useState("9.23%");
+    const [accountAge,setAccountAge] = useState("6 years");
 
     return (
         <>
@@ -30,6 +34,32 @@ function Portfolio(props){
                     </tr>)}
                 </tbody>
                 </table>
+                <div className="buttons"> 
+                    <button type="button" class="btn btn-primary btn-lg">Buy </button>
+                    <button type="button" class="btn btn-primary btn-lg">Sell</button>
+                </div>
+                <h1>Your Statistics</h1>
+                <table class="table">
+                        <tr></tr>
+                        <tr>
+                        <td scope="col">Net Profit</td>
+                        <td scope="col">{netProfit}</td>
+                        </tr>
+                        <tr>
+                        <td scope="col">All Time High</td>
+                        <td scope="col">{allTimeHigh}</td>
+                        </tr>
+                        <tr>
+                        <td scope="col">52 Week High</td>
+                        <td scope="col">{yearHigh}</td>
+                        </tr>
+                        <tr>
+                        <td scope="col">Account Age</td>
+                        <td scope="col">{accountAge}</td>
+                        </tr>
+                </table>
+
+
             </div>
         </>
       )
