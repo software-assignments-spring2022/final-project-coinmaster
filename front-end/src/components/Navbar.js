8f0a50ad858a2c7ac9963879ed99f953a7dcc7b7
Keshnen <keshnen.yoga@gmail.com>
2022-03-21 08:23:07 -0400

    Used Bootstrap display-1
    Updated navbar to use display-1 for heading 1

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,18 +1,17 @@
-import homeIcon from "../media/home-icon.png"
-import logo from "../media/logo.png"
-import "../css/Navbar.css"
+import homeIcon from "../media/home-icon.png";
+import logo from "../media/logo.png";
+import "../css/Navbar.css";
 
 function Navbar(props) {
-    return (
-        
-        <div class="nav-container">
-            <nav class="navbar  navbar-dark bg-primary" >
-                <img src={homeIcon} class="navbar-brand nav-icon" href="Home"/>
-                <h1 class="navbar-brand">Coin Master</h1>
-                <img src={logo} class="navbar-brand nav-icon" href="Home"/>
-            </nav>
-        </div>
-    )
-  }
+  return (
+    <div class="nav-container">
+      <nav class="navbar  navbar-dark bg-primary">
+        <img src={homeIcon} class="navbar-brand nav-icon" href="Home" />
+        <h1 class="navbar-brand display-1">Coin Master</h1>
+        <img src={logo} class="navbar-brand nav-icon" href="Home" />
+      </nav>
+    </div>
+  );
+}
 
-export default Navbar
+export default Navbar;
