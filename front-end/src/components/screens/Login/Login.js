8ad7b3cea25536af8afb831d9a7e7cb0a58d066c
2170chm <2170chm@gmail.com>
2022-04-05 13:06:40 -0400

    Fixing css conflicts

--- front-end/src/components/screens/Login/Login.js
+++ front-end/src/components/screens/Login/Login.js
@@ -9,7 +9,7 @@ const Login = props => {
             <Navbar/>
             <div className="container">
                 <div>
-                    <h1 className="header">Welcome to Coin Master!</h1>
+                    <h1>Welcome to Coin Master!</h1>
                 </div>
                 <LoginForm/>
                 <div className="footnote">
