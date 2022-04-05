3dcbf24053858312d156bea114d7657e4ca3a427
Keshnen <keshnen.yoga@gmail.com>
2022-04-05 13:09:59 -0400

    Buy / Sell Page Forms Working
    Completed Form for Buy and Sell Page
    
    Connection between Express and React Completed (both for Post and Get Requests)

--- front-end/src/Buy.js
+++ front-end/src/Buy.js
@@ -1,11 +1,22 @@
 import "./App.css";
 //import Buy from "./components/screens/Buy/Buy";
 import GetBuy from "./expressConnections/GET/getBuy";
+import BuyForm from "./expressConnections/POST/buyForm";
 
 export default function CoinPage() {
   return (
     <div>
-      <GetBuy/>
+
+      {/* ADD TABLE UP HERE */}
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <BuyForm/>
+      <GetBuy />
     </div>
   );
 }
