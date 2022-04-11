92d4d73e76d3d3950959be1fec7a40b9f52a333a
jlima714 <82290958+jlima714@users.noreply.github.com>
2022-04-11 18:47:27 -0400

    updated compare page

--- front-end/src/Compare.js
+++ front-end/src/Compare.js
@@ -1,83 +1,85 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import "./components/screens/Compare/Compare.css"
-import Button from "./components/screens/Compare/DropdownButton.js";
-import Table from "./components/screens/Compare/CompareTable.js";
-
-class CoinCompare {
-    constructor(price, allTimeHigh, marketCap, rank, type, algo) {
-      this.price = price;
-      this.allTimeHigh = allTimeHigh;
-      this.marketCap = marketCap;
-      this.rank = rank;
-      this.type = type;
-      this.algo = algo;
-    }
-  }
-
-  class PropertyCompare {
-    constructor(Property, coin1, coin2) {
-      this.Property = Property;
-      this.Coin_1 = coin1;
-      this.Coin_2 = coin2;
-    }
-  }
-
-
-  const coin1 = new CoinCompare(
-    9293939,
-    12938749,
-    "12b",
-    2,
-    "Coin",
-    "Algo"
-  ); 
-  
-  const coin2 = new CoinCompare(
-    23923233,
-    932843,
-    "1b",
-    39,
-    "Coin",
-    "Algo"
-  ); 
-
-  const propertiesToCompare = [
-    new PropertyCompare("Price", coin1.price, coin2.price),
-    new PropertyCompare("All Time High", coin1.allTimeHigh, coin2.allTimeHigh),
-    new PropertyCompare("Market Cap", coin1.marketCap, coin2.marketCap),
-    new PropertyCompare("Rank", coin1.rank, coin2.rank),
-    new PropertyCompare("Type", coin1.type, coin2.type),
-    new PropertyCompare("Algorithm", coin1.algo, coin2.algo),
-  ];
+import Comparison from "./components/screens/Compare/Comparison";
+import axios from "axios"
 
 
   function Compare(){
 
+    //stores all coin objects
+    const [messages,setMessage] = useState([]);
+
+    //states for each coin, updated by button click
     const [coin1,setCoin1] = useState("Coin #1");
     const [coin2,setCoin2] = useState("Coin #2");
+
+    //stores all coin names
+    const [coinNames, setCoinNames] = useState([]);
+
+    //states for each coin object selected, updated by button click
+    const [coinTable1, setCoinTable1] = useState({});
+    const [coinTable2, setCoinTable2] = useState({});
+    
+
+    const fetchData = () => {
+   
+      axios
+        .get(`${process.env.REACT_APP_SERVER_HOSTNAME}/compare`)
+        .then(response => {
+
+          const messages = response.data.messages;
+          setMessage(messages);
+
+          const coinNames = response.data.names;
+          setCoinNames(coinNames);
+
+
+        })
+        .catch(err => {
+          console.log("error")
+        })
+    }
+
+    useEffect(() => {
+      // fetch messages this once
+      fetchData()
+  
+      // set a timer to load data from server every n seconds
+      const intervalHandle = setInterval(() => {
+        fetchData()
+      }, 1000000000)
+  
+      // return a function that will be called when this component unloads
+      return e => {
+        // clear the timer, so we don't still load messages when this component is not loaded anymore
+        clearInterval(intervalHandle)
+      } 
+    }, []) // putting a blank array as second argument will cause this function to run only once when component first loads
+
 
     return(
 
         <>
         <div className = 'compare'>
+          <br></br>
+          <br></br>
             
             <h3>Comparison Tool</h3>
             
             {/*Coin 1*/}
-            {Button(coin1,setCoin1)}
+            <Comparison coin={coin1} setCoin={setCoin1} coinNames={coinNames} coinTable={coinTable1} setCoinTable = {setCoinTable1} allCoins={messages}/>
+
+            <br></br>
 
             {/*Coin 2*/}
-            {Button(coin2,setCoin2)}
+            <Comparison coin={coin2} setCoin={setCoin2} coinNames={coinNames} coinTable={coinTable2} setCoinTable = {setCoinTable2} allCoins={messages}/>
 
 
+            <br></br>
             <h3>Performance</h3>
 
             <p>Will be filled in Later</p>
-
-            <h3>Specification</h3>
-
-            {Table(coin1,coin2,propertiesToCompare)}
 
         </div>
         </>
