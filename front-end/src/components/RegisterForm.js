f500ced9605b37d2119db4f3bab022d00c64820e
2170chm <2170chm@gmail.com>
2022-04-18 18:02:36 -0400

    Switching unique classes to ids for organization

--- front-end/src/components/RegisterForm.js
+++ front-end/src/components/RegisterForm.js
@@ -45,47 +45,47 @@ const RegisterForm = props => {
                         document.querySelectorAll(".warning").forEach(curr => {
                             curr.classList.add("hidden")
                           })
-                        document.querySelector(".empty-username-warning").classList.remove("hidden")
+                        document.querySelector("#empty-username-warning").classList.remove("hidden")
                     }else if(error == "your name is required"){
                         document.querySelectorAll(".warning").forEach(curr => {
                             curr.classList.add("hidden")
                           })
-                        document.querySelector(".empty-your-name-warning").classList.remove("hidden")
+                        document.querySelector("#empty-your-name-warning").classList.remove("hidden")
                     }else if(error == "password is required"){
                         document.querySelectorAll(".warning").forEach(curr => {
                             curr.classList.add("hidden")
                           })
-                        document.querySelector(".empty-password-warning").classList.remove("hidden")
+                        document.querySelector("#empty-password-warning").classList.remove("hidden")
                     }else if(error == "must enter password again"){
                         document.querySelectorAll(".warning").forEach(curr => {
                             curr.classList.add("hidden")
                           })
-                        document.querySelector(".empty-confirm-password-warning").classList.remove("hidden")
+                        document.querySelector("#empty-confirm-password-warning").classList.remove("hidden")
                     }else if(error == "email is required"){
                         document.querySelectorAll(".warning").forEach(curr => {
                             curr.classList.add("hidden")
                           })
-                        document.querySelector(".empty-email-warning").classList.remove("hidden")
+                        document.querySelector("#empty-email-warning").classList.remove("hidden")
                     }else if(error == "password must be at least 8 characters"){
                         document.querySelectorAll(".warning").forEach(curr => {
                             curr.classList.add("hidden")
                           })
-                        document.querySelector(".short-password-warning").classList.remove("hidden")
+                        document.querySelector("#short-password-warning").classList.remove("hidden")
                     }else if(error == "passwords must match"){
                         document.querySelectorAll(".warning").forEach(curr => {
                             curr.classList.add("hidden")
                           })
-                        document.querySelector(".unmatched-confirm-password-warning").classList.remove("hidden")
+                        document.querySelector("#unmatched-confirm-password-warning").classList.remove("hidden")
                     }else if(error == "email must be valid"){
                         document.querySelectorAll(".warning").forEach(curr => {
                             curr.classList.add("hidden")
                           })
-                        document.querySelector(".invalid-email-warning").classList.remove("hidden")
+                        document.querySelector("#invalid-email-warning").classList.remove("hidden")
                     }else if(error == "duplicate username"){
                         document.querySelectorAll(".warning").forEach(curr => {
                             curr.classList.add("hidden")
                           })
-                        document.querySelector(".duplicate-username-warning").classList.remove("hidden")
+                        document.querySelector("#duplicate-username-warning").classList.remove("hidden")
                     }
                 }
             });
@@ -95,31 +95,31 @@ const RegisterForm = props => {
             <Form.Group className="mb-3">
                 <Form.Label>Username</Form.Label>
                 <Form.Control name="user_name" placeholder="Enter your name" onChange={handleChange}/>
-                <div className="empty-username-warning warning hidden">You must enter a username</div>
-                <div className="duplicate-username-warning warning hidden">Your username is already taken</div>
+                <div id="empty-username-warning" className="warning hidden">You must enter a username</div>
+                <div id="duplicate-username-warning" className="warning hidden">Your username is already taken</div>
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>Your Name</Form.Label>
                 <Form.Control name="your_name" placeholder="Enter your username" onChange={handleChange}/>
-                <div className="empty-your-name-warning warning hidden">You must enter your name</div>
+                <div id="empty-your-name-warning" className="warning hidden">You must enter your name</div>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" name="password" placeholder="Enter your password" onChange={handleChange}/>
-                <div className="empty-password-warning warning hidden">You must enter a password</div>
-                <div className="short-password-warning warning hidden">Your password must be at least 8 characters</div>
+                <div id="empty-password-warning" className="warning hidden">You must enter a password</div>
+                <div id="short-password-warning" className="warning hidden">Your password must be at least 8 characters</div>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Confirm Password</Form.Label>
                 <Form.Control type="password" name="confirm_password" placeholder="Enter your password again" onChange={handleChange}/>
-                <div className="empty-confirm-password-warning warning hidden">You must re-enter your password</div>
-                <div className="unmatched-confirm-password-warning warning hidden">Your passwords must match</div>
+                <div id="empty-confirm-password-warning" className="warning hidden">You must re-enter your password</div>
+                <div id="unmatched-confirm-password-warning" className="warning hidden">Your passwords must match</div>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email</Form.Label>
                 <Form.Control type="email" name="email" placeholder="Enter your email" onChange={handleChange}/>
-                <div className="empty-email-warning warning hidden">You must enter an email</div>
-                <div className="invalid-email-warning warning hidden">You must enter a valid email (e.g. example@exapmle.com)</div>
+                <div id="empty-email-warning" className="warning hidden">You must enter an email</div>
+                <div id="invalid-email-warning" className="warning hidden">You must enter a valid email (e.g. example@exapmle.com)</div>
             </Form.Group>
             <Button variant="dark" type="submit" onClick={handleSubmit}>
                 Register
