ebc8bcd94df3c41456707c2c01df9141eaea1cf9
2170chm <2170chm@gmail.com>
2022-04-30 19:54:36 -0400

    update README and update tests

--- back-end/test/api_table_test.js
+++ back-end/test/api_table_test.js
@@ -1,58 +1,58 @@
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-// const server = require('../app');
-// const should = chai.should();
-// const expect = chai.expect;
-// chai.use(chaiHttp);
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+const should = chai.should();
+const expect = chai.expect;
+chai.use(chaiHttp);
 
 // TESTING FROM BUY ROUTE
-// describe('get request to get API data with all fields from API', () => { 
-//     it('it should successfully get the crypto and quantity to backend and receive a http 200 status code', (done) => {
-//         const test_buy = {
-//             crypto: "Bitcoin",
-//             quantity: "5",
-//             cryptoData: "allCoins",
-//             status: 'all good',
-//         }
-//         chai.request(server) 
-//         .get('/buy') 
-//         .query(test_buy)
-//         .end((err, res) => { 
-//             res.should.have.status(200); 
-//             res.body.should.be.a("object")
-//             res.body.should.have.property("success")
-//             res.body.should.have.property("crypto")
-//             res.body.should.have.property("quantity")
-//             res.body.should.have.property("cryptoData")
-//             res.body.success.should.eql(true)
-//             expect(res.body.cryptoData).to.be.an('array');
-//             done() 
-//         })
-//     })
-// })
+describe('get request to get API data with all fields from API', () => { 
+    it('it should successfully get the crypto and quantity to backend and receive a http 200 status code', (done) => {
+        const test_buy = {
+            crypto: "Bitcoin",
+            quantity: "5",
+            cryptoData: "allCoins",
+            status: 'all good',
+        }
+        chai.request(server) 
+        .get('/buy') 
+        .query(test_buy)
+        .end((err, res) => { 
+            res.should.have.status(200); 
+            res.body.should.be.a("object")
+            res.body.should.have.property("success")
+            res.body.should.have.property("crypto")
+            res.body.should.have.property("quantity")
+            res.body.should.have.property("cryptoData")
+            res.body.success.should.eql(true)
+            expect(res.body.cryptoData).to.be.an('array');
+            done() 
+        })
+    })
+})
 
-// // TESTING FROM SELL ROUTE
-// describe('get request to get API data with all fields from API', () => { 
-//     it('it should successfully get the crypto and quantity to backend and receive a http 200 status code', (done) => {
-//         const test_buy = {
-//             crypto: "Bitcoin",
-//             quantity: "5",
-//             cryptoData: "allCoins",
-//             status: 'all good',
-//         }
-//         chai.request(server) 
-//         .get('/sell') 
-//         .query(test_buy)
-//         .end((err, res) => { 
-//             res.should.have.status(200); 
-//             res.body.should.be.a("object")
-//             res.body.should.have.property("success") 
-//             res.body.should.have.property("crypto")
-//             res.body.should.have.property("quantity")
-//             res.body.should.have.property("cryptoData")
-//             res.body.success.should.eql(true)
-//             expect(res.body.cryptoData).to.be.an('array');
-//             done() 
-//         })
-//     })
-// })
+// TESTING FROM SELL ROUTE
+describe('get request to get API data with all fields from API', () => { 
+    it('it should successfully get the crypto and quantity to backend and receive a http 200 status code', (done) => {
+        const test_buy = {
+            crypto: "Bitcoin",
+            quantity: "5",
+            cryptoData: "allCoins",
+            status: 'all good',
+        }
+        chai.request(server) 
+        .get('/sell') 
+        .query(test_buy)
+        .end((err, res) => { 
+            res.should.have.status(200); 
+            res.body.should.be.a("object")
+            res.body.should.have.property("success") 
+            res.body.should.have.property("crypto")
+            res.body.should.have.property("quantity")
+            res.body.should.have.property("cryptoData")
+            res.body.success.should.eql(true)
+            expect(res.body.cryptoData).to.be.an('array');
+            done() 
+        })
+    })
+})
