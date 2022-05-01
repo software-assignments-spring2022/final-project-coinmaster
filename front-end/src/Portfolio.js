a62cd755095c8458cfc272967271c97e63ba1d5a
jlima714 <82290958+jlima714@users.noreply.github.com>
2022-05-01 06:41:26 -0400

    added styling

--- front-end/src/Portfolio.js
+++ front-end/src/Portfolio.js
@@ -1,5 +1,5 @@
  import "./App.css";
-import Porfolio from "./components/screens/Portfolio/Portfolio";
+ import "./css/Portfolio.css"
 import GetPortfolio from "./expressConnections/GET/getPortfolio";
 
 import "./portfolio.css"
@@ -8,13 +8,6 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Portfolio(props) {
-  /* const [ownedCryptos, setOwnedCryptos] = useState([
-    ["1", "Lorem", "$100.00", "$100M"],
-  ]);
-  const [netProfit, setNetProfit] = useState("5.34%");
-  const [allTimeHigh, setAllTimeHigh] = useState("12.44%");
-  const [yearHigh, setYearHigh] = useState("9.23%");
-  const [accountAge, setAccountAge] = useState("6 years"); */
 
   return (
     <div>
@@ -23,8 +16,8 @@ function Portfolio(props) {
       <GetPortfolio />
 
       <div className = "PortfolioInLine">
-        <Link to="/buy"> <button className="PortfolioButton btn btn-dark btn ">Buy</button> </Link>
-        <Link to="/sell"> <button className="PortfolioButton btn btn-dark btn">Sell</button> </Link>
+        <Link to="/buy"> <button id="button1" className="PortfolioButton btn btn-dark btn">Buy</button> </Link>
+        <Link to="/sell"> <button id="button2" className="PortfolioButton btn btn-dark btn">Sell</button> </Link>
         
 </div>
 
