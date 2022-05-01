645c8305424922e5d61418281c264a8f6f062fdc
jlima714 <82290958+jlima714@users.noreply.github.com>
2022-05-01 06:40:23 -0400

    cleaned up code

--- front-end/src/expressConnections/GET/getBuy.js
+++ front-end/src/expressConnections/GET/getBuy.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import CryptoTable from "../../components/CryptoTable"
+import "../../css/Portfolio.css"
 
 const GetBuy = () => {
   const [messages, setMessages] = useState([])
@@ -15,7 +16,6 @@ const GetBuy = () => {
     axios
       .get(`${process.env.REACT_APP_SERVER_HOSTNAME}/buy`)
       .then(response => {
-        // axios bundles up all response data in response.data property
         const messages = response.data
 
         let cryptoTableData = [];
@@ -30,37 +30,14 @@ const GetBuy = () => {
           })
         })
 
-
-        /* let wanted_1 = "Bitcoin";
-        let wanted_2 = "Tether";
-
-        function filterByCoin(check_coin) {
-          if ( check_coin.name == wanted_1 || check_coin.name == wanted_2) {
-            return true
-          }
-        }
-        
-        let filteredCryptoTableData = cryptoTableData.filter(filterByCoin) */
-
         setCryptoTableData(cryptoTableData);
-        //setCryptoTableData(filteredCryptoTableData);
-        setMessages(messages)
-
-        //setCryptoTableData(messages.cryptoData);
       })
       
       .catch(err => {
         setError(err)
       })
-      .finally(() => {
-        // the response has been received, so remove the loading icon
-        setLoaded(true)
-      })
   }
-
-    // set up loading data from server when the component first loads
     useEffect(() => {
-    // fetch messages this once
     fetchMessages()
 
     const intervalHandle = setInterval(() => {
@@ -74,12 +51,10 @@ const GetBuy = () => {
 
   return (
     <>
-
-    <br></br>
+    <div id="table">
     <CryptoTable data={cryptoTableData} columns={tableColumns}/>
-    Buy Crypto: {messages.crypto}
+    </div>
     <br></br>
-    Buy Quantity: {messages.quantity}
     </>
   )
 }
