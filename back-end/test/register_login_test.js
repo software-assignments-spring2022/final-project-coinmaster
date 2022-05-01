609970a9019993f7a6fef3820be0b88ff3ffcb44
2170chm <2170chm@gmail.com>
2022-05-01 03:37:09 -0400

    Updated unit tests and fixed bugs brought by new code

--- back-end/test/register_login_test.js
+++ back-end/test/register_login_test.js
@@ -241,6 +241,7 @@ describe('post request to buy data with all fields', () => {
         const test_buy = {
             crypto: "BTC",
             quantity: 5,
+            user: 'unittest',
         }
         chai.request(server) 
         .post('/buy') 
@@ -256,58 +257,64 @@ describe('post request to buy data with all fields', () => {
     })
 })
 
-// describe('post request to buy data with invalid coin', () => { 
-//     it('it should successfully post the invalid crypto to backend and receive a http 200 status code', (done) => {
-//         const test_buy = {
-//             crypto: "DOESNOTEXIST",
-//             quantity: 5,
-//         }
-//         chai.request(server) 
-//         .post('/buy') 
-//         .send(test_buy)
-//         .end((err, res) => { 
-//             res.should.have.status(200); 
-//             res.body.should.be.a("object") 
-//             res.body.should.have.property("success")
-//             res.body.success.should.eql(false) 
-//             res.body.message.should.eql("That is not a valid coin") 
-//             done() 
-//         })
-//     })
-// })
-
-// //portfolio
-//TESTING FROM PORTFOLIO ROUTE
-describe('get request to portfolio page', () => { 
-    it('it should successfully get the message from backend on portfolio route and receive a http 200 status code', (done) => {
+describe('post request to buy data with invalid coin', () => { 
+    it('it should successfully post the invalid crypto to backend and receive a http 200 status code', (done) => {
+        const test_buy = {
+            crypto: "DOESNOTEXIST",
+            quantity: 5,
+            user: 'unittest',
+        }
         chai.request(server) 
-        .get('/portfolio') 
+        .post('/buy') 
+        .send(test_buy)
         .end((err, res) => { 
-            res.should.have.status(200);
+            res.should.have.status(200); 
+            res.body.should.be.a("object") 
+            res.body.should.have.property("success")
+            res.body.success.should.eql(false) 
+            res.body.message.should.eql("That is not a valid coin") 
             done() 
         })
     })
 })
 
-// describe('post request to sell data with all fields', () => { 
-//     it('it should successfully post the crypto and quantity to backend and receive a http 200 status code', (done) => {
-//         const test_sell = {
-//             crypto: "BTC",
-//             quantity: 1,
-//         }
-//         chai.request(server) 
-//         .post('/sell') 
-//         .send(test_sell)
-//         .end((err, res) => { 
-//             res.should.have.status(200); 
-//             res.body.should.be.a("object") 
-//             res.body.should.have.property("success")
-//             res.body.success.should.eql(true) 
-//             res.body.message.should.eql("Congratulations, you sold 5 units of BTC") 
-//             done() 
-//         })
-//     })
-// })
+// //portfolio
+//TESTING FROM PORTFOLIO ROUTE
+describe('post request to portfolio page', () => { 
+    it('it should successfully post to portfolio route and receive a http 200 status code', (done) => {
+        const test_portfolio = {
+            user: 'unittest',
+        }
+        chai.request(server) 
+        .post('/portfolio')
+        .send(test_portfolio)
+        .end((err, res) => { 
+            res.should.have.status(200);
+            done()
+        })
+    })
+})
+
+describe('post request to sell data with all fields', () => { 
+    it('it should successfully post the crypto and quantity to backend and receive a http 200 status code', (done) => {
+        const test_sell = {
+            crypto: "BTC",
+            quantity: 5,
+            user: 'unittest',
+        }
+        chai.request(server) 
+        .post('/sell') 
+        .send(test_sell)
+        .end((err, res) => { 
+            res.should.have.status(200); 
+            res.body.should.be.a("object") 
+            res.body.should.have.property("success")
+            res.body.success.should.eql(true) 
+            res.body.message.should.eql("Congratulations, you sold 5 units of BTC") 
+            done() 
+        })
+    })
+})
 
 describe('post request to login page with empty username', () => { 
     it('it should successfully post the empty username to backend and receive a http 400 status code', (done) => {
@@ -383,6 +390,26 @@ describe('post request to login page with incorrect username', () => {
             res.body.should.have.property("error")
             res.body.should.have.property("message")
             res.body.message.should.eql("username does not exist")  
+            expect(res).to.have.status(400)
+            done() 
+        })
+    })
+})
+
+describe('post request to login page with incorrect password', () => { 
+    it('it should successfully post the incorrect username and password to backend and receive a http 400 status code', (done) => {
+        const test_user = {
+            user_name: "unittest",
+            password: "wrongpassword",
+        }
+        chai.request(server) 
+        .post('/login') 
+        .send(test_user)
+        .end((err, res) => {
+            res.body.should.be.a("object")
+            res.body.should.have.property("error")
+            res.body.should.have.property("message")
+            res.body.message.should.eql("incorrect password")  
             expect(res).to.have.status(400)
             done() 
         })
