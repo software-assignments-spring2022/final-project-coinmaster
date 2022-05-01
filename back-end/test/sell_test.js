5274476caba51f0be6f4f41cfd58b0d1e4624b2f
2170chm <2170chm@gmail.com>
2022-05-01 00:29:10 -0400

    Updated tests

--- back-end/test/sell_test.js
+++ back-end/test/sell_test.js
@@ -5,27 +5,6 @@ const should = chai.should();
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-// describe('post request to sell data with all fields', () => { 
-//     it('it should successfully post the crypto and quantity to backend and receive a http 200 status code', (done) => {
-//         const test_sell = {
-//             crypto: "test",
-//             quantity: 10,
-//         }
-//         chai.request(server) 
-//         .post('/sell') 
-//         .send(test_sell)
-//         .end((err, res) => { 
-//             res.should.have.status(200); 
-//             res.body.should.be.a("object") 
-//             res.body.should.have.property("success")
-//             res.body.success.should.eql(true) 
-//             res.body.message.should.eql("sell data post success") 
-//             done() 
-//         })
-//     })
-// })
-
-
 describe('get request to sell data', () => { 
     it('it should successfully get sell page receive a http 200 status code', (done) => {
         chai.request(server) 
