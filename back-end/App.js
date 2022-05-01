74588056321f40b27fdd8aba619b1bffd6211dda
2170chm <2170chm@gmail.com>
2022-05-01 03:08:43 -0400

    Updated App.js and tests

--- back-end/App.js
+++ back-end/App.js
@@ -86,7 +86,7 @@ app.get('/portfolio', async (req, res) => {
     console.error(err)
     res.status(400).json({
       error: err,
-      message: 'failed to work',
+      message: err.message,
     })
   }
 })
