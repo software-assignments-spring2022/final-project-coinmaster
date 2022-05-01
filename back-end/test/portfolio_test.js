9e4c2787edf810049895e4b2d50f9e468205e1d0
2170chm <2170chm@gmail.com>
2022-05-01 01:00:40 -0400

    Updating tests, testing Circle CI

--- back-end/test/portfolio_test.js
+++ back-end/test/portfolio_test.js
@@ -1,8 +1,1 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../App');
-const should = chai.should();
-const expect = chai.expect;
-chai.use(chaiHttp);
-
-// TESTING FROM PORTFOLIO ROUTE
+// TESTING FROM PORTFOLIO ROUTE. Currently unused, code moved to register_login_test.js
