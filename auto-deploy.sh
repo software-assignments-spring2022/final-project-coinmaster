7477c4bcd17218ffae6c4b2afddc6bcec0757f44
2170chm <2170chm@gmail.com>
2022-05-02 17:39:53 -0400

    Updated auto-deploy script

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ssh root@67.207.83.112 << EOF
+yes | /opt/MNG/MNGVIEWHP/fe/uninstall
 cd final-project-coinmaster
 git pull origin master
 cd front-end && npm install && npm run build && rm -rf ../back-end/client && cp -R ./build ../back-end/client
