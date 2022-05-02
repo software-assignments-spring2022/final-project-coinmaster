03fbcdf899a56dd0bb3ceaa2d93a867181dd35bb
Olivia Wang <lw2694@nyu.edu>
2022-05-02 15:24:52 -0400

    Update Video.js

--- front-end/src/components/screens/Learn/Video.js
+++ front-end/src/components/screens/Learn/Video.js
@@ -5,11 +5,14 @@ import './Learn.css'
 export default function Video() {
   return (
     <div className="Video">
-      <h4>What is Bitcoin?</h4>
+      <h4>Basics: What is Bitcoin?</h4>
       <YoutubeEmbed embedId="41JCpzvnn_0" />
-{/*       <br></br>
-      <h4>Simple $100 a Day Crypto Trading Strategy Anyone Can Use</h4>
-      <YoutubeEmbed embedId="bxnZEgtHf5E" /> */}
+        <br></br>
+      <h4>Tutorial: The Ultimate Cryptocurrency Trading Course for Beginners</h4>
+      <YoutubeEmbed embedId="mQvw5JXXnrQ" /> 
+      <br></br>
+      <h4>Basics: Blockchain In 7 Minutes</h4>
+      <YoutubeEmbed embedId="yubzJw0uiE4" /> 
     </div>
   );
 }
