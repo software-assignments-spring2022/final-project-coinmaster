27b52767c325db4ae33fdc1f64ab648e87e9abf7
2170chm <2170chm@gmail.com>
2022-05-02 17:36:12 -0400

    Updated auto-deploy script

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no "root@67.207.83.112"
-ssh root@67.207.83.112 <<EOF
+ssh root@67.207.83.112 << EOF
 cd final-project-coinmaster
 git pull origin master
 cd front-end && npm install && npm run build && rm -rf ../back-end/client && cp -R ./build ../back-end/client
