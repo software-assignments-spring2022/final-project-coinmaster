abb940bc3e9b74b2361711e672e194c938fcc54a
Olivia Wang <lw2694@nyu.edu>
2022-05-02 17:01:58 -0400

    Update auto-deploy.sh

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#ssh todo
+ssh root@67.207.83.112 <<EOF
+cd final-project-coinmaster
 git pull origin master
 cd front-end && npm install && npm run build && rm -rf ../back-end/client && cp -R ./build ../back-end/client
-pm2 start npm -- start
 cd ../back-end && npm install && pm2 start npm -- start
+EOF
