928e95b9776c2b82d6d03a804acbef82224a5f27
Keshnen <keshnen.yoga@gmail.com>
2022-05-03 14:55:37 -0400

    Updated auto-deploy

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -2,7 +2,7 @@
 ssh -o StrictHostKeyChecking=no "root@67.207.83.112" << EOF
 ssh root@67.207.83.112
 cd final-project-coinmaster
-git stash save --keep-index
+git reset --hard
 git pull origin master
 cd front-end && npm install && npm run build && rm -rf ../back-end/client && cp -R ./build ../back-end/client
 cd ../back-end && npm install && pm2 start npm -- start
